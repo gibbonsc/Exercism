93a5aad25fe48eca834e63dcab461d049d89168a
C Gibbons <gibbonsc@byui.edu>
2026-02-12 22:48:41 -0700

    jq assembly-line, jq two-fer completed

---
+++ jq/two-fer/two-fer.jq
@@ -0,0 +1,9 @@
+"One for \( .name // "you" ), one for me."
+
+# You might want to look at:
+#
+# - the alternative operator:
+#   https://jqlang.github.io/jq/manual/v1.7/#alternative-operator
+#
+# - string interpolation:
+#   https://jqlang.github.io/jq/manual/v1.7/#string-interpolation
---
+++ powershell/two-fer/TwoFer.ps1
@@ -0,0 +1,30 @@
+Function Get-TwoFer(){
+    [CmdletBinding()]
+    Param(
+        [string]$Name = "you"
+    )
+
+    Return "One for $Name, one for me"
+
+    <#
+    .SYNOPSIS
+    "Two-fer" is short for two for one. One for you and one for me.
+    
+    .DESCRIPTION
+    If the given name is "Alice", the result should be "One for Alice, one for me."
+    If no name is given, the result should be "One for you, one for me."
+    
+    .PARAMETER Name
+    The name to use.
+    
+    .EXAMPLE
+    Get-TwoFer
+
+    Will return: One for you, one for me
+
+    .EXAMPLE
+    Get-TwoFer -Name Alice
+    
+    Will return: One for Alice, one for me
+    #>
+}
