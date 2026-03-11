50709f85cff288a6b6463cd8a913c3847b1c28a6
C Gibbons <gibbonsc@byui.edu>
2026-03-11 14:57:34 -0600

    fsharp/space-age completed

---
+++ fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,27 @@
+module SpaceAge
+
+type Planet =
+| Mercury
+| Venus
+| Earth
+| Mars
+| Jupiter
+| Saturn
+| Uranus
+| Neptune
+
+let age (planet: Planet) (seconds: int64): float =
+    let orbitalPeriod : float =
+        match planet with
+        | Mercury -> 0.2408467
+        | Venus -> 0.61519726
+        | Earth -> 1.0
+        | Mars -> 1.8808158
+        | Jupiter -> 11.862615
+        | Saturn -> 29.447498
+        | Uranus -> 84.016846
+        | Neptune -> 164.79132
+    let adjustedAgeInSeconds = float seconds / orbitalPeriod
+    let secondsInEarthYear = float 31_557_600.0
+    let adjustedAgeInYears = adjustedAgeInSeconds / secondsInEarthYear
+    adjustedAgeInYears
