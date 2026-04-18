6a9b89f10dc5232386efb8c6980105b17a5ecba4
C Gibbons <gibbonsc@byui.edu>
2026-04-18 06:09:55 -0600

    csharp/cars-assemble completed

---
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -0,0 +1,17 @@
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if (speed == 0) { return 0.0; }
+        else if (speed < 5) { return 1.0; }
+        else if (speed < 9) { return 0.9; }
+        else if (speed < 10) { return 0.8; }
+        return 0.77;
+    }
+
+    public static double ProductionRatePerHour(int speed)
+        => 221 * speed * SuccessRate(speed);
+
+    public static int WorkingItemsPerMinute(int speed)
+        => (int)(ProductionRatePerHour(speed) / 60);
+}
