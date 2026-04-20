8e6ae91ba8668dbf882f8fd60f1bbae65a76d9fd
C Gibbons <gibbonsc@byui.edu>
2026-04-20 17:12:38 -0600

    Leveraged csharp/hello-world as a sandbox

--- csharp/hello-world/HelloWorld.cs
+++ csharp/hello-world/HelloWorld.cs
@@ -1,4 +1,10 @@
 public static class HelloWorld
 {
     public static string Hello() => "Hello, Worldex!";
+
+    public static void SandboxCruft()
+    {
+        List<double> numbers = [0.0, 2.78, 3.14];
+        int size = numbers.Count;
+    }
 }
