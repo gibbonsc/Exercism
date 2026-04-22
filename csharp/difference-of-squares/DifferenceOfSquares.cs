730affd528a44f4d875b7c80a59b57bea505fe1e
C Gibbons <gibbonsc@byui.edu>
2026-04-22 10:18:19 -0600

    csharp/difference-of-squares completed onece (simple algorithm)

---
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,27 @@
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        int result = 0;
+        for (int i = 0; i <= max; i++)
+        {
+            result += i;
+        }
+        return result * result;
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        int result = 0;
+        for (int i = 0; i <= max; i++)
+        {
+            result += i * i;
+        }
+        return result;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
