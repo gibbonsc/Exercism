673acc529285901f231477742902e4caa00451f9
C Gibbons <gibbonsc@byui.edu>
2026-05-01 00:42:39 -0600

    sandboxing csharp/hello-world

--- csharp/hello-world/HelloWorld.cs
+++ csharp/hello-world/HelloWorld.cs
@@ -1,10 +1,16 @@
 public static class HelloWorld
 {
-    public static string Hello() => "Hello, Worldex!";
+    public static string Hello() => "Hello, World!";
 
     public static void SandboxCruft()
     {
+        // just trying out some unrelated code
         List<double> numbers = [0.0, 2.78, 3.14];
         int size = numbers.Count;
     }
 }
+
+/*
+    Previous message that didn't pass the unit test:
+    "Goodbye Mars!"
+*/
